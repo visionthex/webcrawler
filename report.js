afd6061818638264334ef8ac1267fca9eefc783e
visionthex <sandseclos@gmail.com>
2024-07-30 04:56:58 -0400

    CSV export and colorized for ouput visuals

--- report.js
+++ report.js
@@ -1,30 +1,51 @@
-function printReport(pages){
+const { blue, green, yellow, cyan, white, magenta, red } = require('colorette');
+const fs = require('fs');
+const path = require('path');
+
+function printReport(pages) {
   console.log('')
   console.log('')
-  console.log('========================')
-  console.log('WEBCRAWLER REPORT')
-  console.log('========================')
+  console.log(blue('========================'))
+  console.log(green('WEBCRAWLER REPORT'))
+  console.log(blue('========================'))
+  console.log(magenta(`Report generated on: ${new Date().toLocaleString()}`))
+  console.log(blue('========================'))
+
   const sortedPages = sortPages(pages)
-  for (const sortedPage of sortedPages){
+  sortedPages.forEach((sortedPage, index) => {
     const url = sortedPage[0]
     const hits = sortedPage[1]
-    console.log(`Webpage: ${url}, Number of hits: ${hits}`)
-  }
-  console.log('========================')
-  console.log('END OF REPORT')
-  console.log('========================')
+    let color = yellow
+    if (index < 3) {
+      color = red
+    }
+    console.log(`${color('Webpage:')} ${cyan(url)}, ${color('Number of hits:')} ${white(hits)}`)
+  })
+
+  console.log(blue('========================'))
+  console.log(green('END OF REPORT'))
+  console.log(blue('========================'))
   console.log('')
   console.log('')
+
+  exportReportToCSV(sortedPages);
 }
 
 function sortPages(pages) {
   const pagesArr = Object.entries(pages)
-  pagesArr.sort((a,b) => {
-    aHits = a[1]
-    bHits = b[1]
-    return b[1] - a[1]
+  pagesArr.sort((a, b) => {
+    const aHits = a[1]
+    const bHits = b[1]
+    return bHits - aHits
   })
   return pagesArr
+}
+
+function exportReportToCSV(sortedPages) {
+  const csvContent = sortedPages.map(page => `${page[0]},${page[1]}`).join('\n');
+  const filePath = path.join(__dirname, 'report.csv');
+  fs.writeFileSync(filePath, csvContent);
+  console.log(green(`Report exported to ${filePath}`));
 }
 
 module.exports = {
